646efc2bb704e3d189594757bdfa6714b469eb6d
ho-ryu-ji <ryuta.soc7@gmail.com>
2020-12-24 04:49:10 +0900

    ToDo

--- src/App.js
+++ src/App.js
@@ -1,48 +1,77 @@
 import React, { useState } from "react";
+import { nanoid } from "nanoid";
 import './App.css';
 import Todo from "./components/Todo";
 import Form from "./components/Form";
 import FilterButton from "./components/FilterButton";
 
-function App(props) {
+export default function App(props) {
   const [tasks, setTasks] = useState(props.tasks);
 
   function addTask(name) {
-    const newTask = { id: "id", name: name, completed: false };
+    const newTask = { id: "todo-" + nanoid(), name: name, completed: false };
     setTasks([...tasks, newTask]);
   }
 
-  const taskList = tasks.map(task => (
-    <Todo
-      id={task.id}
-      name={task.name}
-      completed={task.completed}
-      key={task.id}
-    />
-  )
-  );
+  function deleteTask(id) {
+    const remainingTasks = tasks.filter(task => id !== task.id);
+    setTasks(remainingTasks);
+  }
 
-  return (
-    <div className="todoapp stack-large">
-      <h1>TodoMatic</h1>
-      <Form addTask={addTask} />
-      <div className="filters btn-group stack-exception">
-        <FilterButton />
-        <FilterButton />
-        <FilterButton />
-      </div>
-      <h2 id="list-heading">
-        3 tasks remaining
-        </h2>
-      <ul
-        role="list"
-        className="todo-list stack-large stack-exception"
-        aria-labelledby="list-heading"
-      >
-        {taskList}
-      </ul>
+  function editTask(id, newName) {
+    const editedTaskList = tasks.map(task => {
+        if (id === task.id) {
+          return {...task, name: newName}
+        }
+        return task;
+      });
+      setTasks(editedTaskList);
+  }
+
+  function toggleTaskCompleted(id) {
+    console.log(tasks[0])
+    const updatedTasks = tasks.map(task => {
+      if (id === task.id) {
+        return { ...task, completed: !task.completed }
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+
+const taskList = tasks.map(task => (
+  <Todo
+    id={task.id}
+    name={task.name}
+    completed={task.completed}
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    editTask={editTask}
+  />
+)
+);
+
+const tasksNoun = taskList.length !== 1 ? 'tasks' : 'task';
+const headingText = `${taskList.length} ${tasksNoun} remaining`;
+
+return (
+  <div className="todoapp stack-large">
+    <h1>TodoMatic</h1>
+    <Form addTask={addTask} />
+    <div className="filters btn-group stack-exception">
+      <FilterButton />
+      <FilterButton />
+      <FilterButton />
     </div>
-  );
+    <h2 id="list-heading">{headingText}</h2>
+    <ul
+      role="list"
+      className="todo-list stack-large stack-exception"
+      aria-labelledby="list-heading"
+    >
+      {taskList}
+    </ul>
+  </div>
+);
 }
-
-export default App;
